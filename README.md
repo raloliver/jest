56a3cc9af1f19a4440d052b40eaf0e4f27feb21b
ilimaoliveira <israel.limaoliveira@altran.com>
2020-11-11 21:08:34 +0000

    add prettier

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Criado pelo Facebook.
 > ASSERTIONS: determinar se o resultado de um método, é exatamente o que estou esperando.
 
 `npm test`
+
 - testMach: regex que busca recursivamente arquivos de test (inclusive jsx) dentro da pasta 'tests' ou fora dela.
 
 > Para evitar falso positivo, utilize o método `.not` do `expect`. É possível ter mais de um `expect` dentro de um `it`
@@ -15,7 +16,7 @@ Criado pelo Facebook.
 
 - https://marketplace.visualstudio.com/items?itemName=andys8.jest-snippets
 
-### Watch 
+### Watch
 
 - Olha apenas o que foi alterado, e não todos os testes: `jest --watchAll`
 
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,13 +1,11 @@
-const { queryString } = require("./queryString");
+const {queryString} = require('./queryString');
 
 describe('Object to query string', () => {
-    it('should create a valid query string when an object is provided', () => {
-        const obj = {
-            name: 'Roger Rabbit',
-            company: 'ACME'
-        };
-        expect(queryString(obj)).toBe(
-            'name=Roger Rabbit&company=ACME'
-        );
-    });
+  it('should create a valid query string when an object is provided', () => {
+    const obj = {
+      name: 'Roger Rabbit',
+      company: 'ACME',
+    };
+    expect(queryString(obj)).toBe('name=Roger Rabbit&company=ACME');
+  });
 });
