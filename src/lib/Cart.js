cb3cc3b331edfcad15c72eb757ab4cc0c1fc7f43
Israel Oliveira <raloliver.com@gmail.com>
2021-01-05 18:36:50 +0000

    should ensure only one type of product on the cart

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,8 +1,19 @@
+import find from 'lodash/find';
+import remove from 'lodash/remove';
+
 export default class Cart {
   items = [];
+
   add(item) {
+    const itemDuplicated = {product: item.product};
+
+    if (find(this.items, itemDuplicated)) {
+      remove(this.items, itemDuplicated);
+    }
+
     this.items.push(item);
   }
+
   getTotal() {
     return this.items.reduce((acc, item) => {
       return acc + item.quantity * item.product.price;
--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -2,23 +2,39 @@ import Cart from './Cart';
 
 describe('Cart', () => {
   let cart;
+  let product = {
+    title: 'Nintendo Switch',
+    price: 26325,
+  };
+
   beforeEach(() => {
     cart = new Cart();
   });
+
   it('should return 0 when getTotal() init', () => {
     const cart = new Cart();
     expect(cart.getTotal()).toEqual(0);
   });
+
   it('should multiply quantity and price and return the total amount', () => {
     const item = {
-      product: {
-        title: 'Nintendo Switch',
-        price: 26325,
-      },
+      product,
       quantity: 2,
     };
 
     cart.add(item);
     expect(cart.getTotal()).toEqual(52650);
+  });
+
+  it('should ensure only one type of product on the cart', () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+    cart.add({
+      product,
+      quantity: 1,
+    });
+    expect(cart.getTotal()).toEqual(26325);
   });
 });
