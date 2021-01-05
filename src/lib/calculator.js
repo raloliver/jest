c821ea143ce7d568b20a30d9fce2731b100784ab
Israel Oliveira <raloliver.com@gmail.com>
2021-01-05 17:28:00 +0000

    refactor calculate

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,12 +1,12 @@
 module.exports.sum = (num1, num2) => {
   // when you use + instead parseInt, the empty string was converted in a 0
   const Num1 = parseInt(num1, 10);
-  const Num2 = +num2;
+  const Num2 = parseInt(num2, 10);
   //verify if number exists
   if (Number.isNaN(Num1) || Number.isNaN(Num2)) {
     throw new Error('Check input');
   }
 
   // add + before variable to convert in a number
-  return +num1 + +num2;
+  return Num1 + Num2;
 };
