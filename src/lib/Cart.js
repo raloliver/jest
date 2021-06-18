bee08515690a053d6d0cf5c198e56225d33bcc4b
raloliver <contato@raloliver.com.br>
2021-06-18 15:25:24 +0100

    finish dinero feature

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -2,53 +2,14 @@
  * File: Cart.js
  * Project: jest-app
  * Created: Thursday, March 11th 2021, 5:12:26 pm
- * Last Modified: Friday, June 18th 2021, 3:17:58 pm
+ * Last Modified: Friday, June 18th 2021, 3:22:59 pm
  * Copyright © 2021 AMDE Agência
  */
 
 import find from 'lodash/find';
 import remove from 'lodash/remove';
 import Dinero from 'dinero.js';
-
-const calculateDiscountByPercentage = (amount, {condition, quantity}) => {
-  if (condition?.percentage && quantity > condition.minimum) {
-    return amount.percentage(condition.percentage);
-  }
-
-  return Money({amount: 0});
-};
-
-const calculateDiscountByQuantity = (amount, {condition, quantity}) => {
-  const isEven = quantity % 2 === 0;
-
-  if (condition?.quantity && quantity > condition.quantity) {
-    return amount.percentage(isEven ? 50 : 40);
-  }
-
-  return Money({amount: 0});
-};
-
-const calculateDiscount = (amount, quantity, condition) => {
-  const conditionList = Array.isArray(condition) ? condition : [condition];
-
-  const [higherDiscount] = conditionList
-    .map(conditionItem => {
-      if (conditionItem.percentage) {
-        return calculateDiscountByPercentage(amount, {
-          condition: conditionItem,
-          quantity,
-        }).getAmount();
-      } else if (conditionItem.quantity) {
-        return calculateDiscountByQuantity(amount, {
-          condition: conditionItem,
-          quantity,
-        }).getAmount();
-      }
-    })
-    .sort((discount1, discount2) => discount2 - discount1);
-
-  return Money({amount: higherDiscount});
-};
+import {calculateDiscount} from './discount.utils';
 
 const Money = Dinero;
 
